5a6e1c2ff6d357af8649670a00231370e2410c4a
mateolopezlorenz <mlopezlorenz@cifpfbmoll.eu>
2026-02-06 19:08:15 +0100

    Se ha añadido 'UserRepository'

--- Backend/daw2026/src/main/java/daw2026/Repository/UserRepository.java
+++ Backend/daw2026/src/main/java/daw2026/Repository/UserRepository.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 import daw2026.Model.user;
 
 public interface UserRepository extends JpaRepository<user, Long> {
-    Optional<user> findByEmail(String email);
+    Optional<user> findByUsername(String username);
     Optional<user> findById(Long id);
     List<user> findAll();
     
